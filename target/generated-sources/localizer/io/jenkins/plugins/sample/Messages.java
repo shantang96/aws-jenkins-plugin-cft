89768bb567da0bbe4da9ac23ac7a3554493d7de8
Shantanu Gupta <shantang@usc.edu>
2018-07-09 12:04:24 -0500

    working demo

--- target/generated-sources/localizer/io/jenkins/plugins/sample/Messages.java
+++ target/generated-sources/localizer/io/jenkins/plugins/sample/Messages.java
@@ -26,47 +26,25 @@ public class Messages {
     private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
 
     /**
-     * Key {@code HelloWorldBuilder.DescriptorImpl.errors.missingName}:
-     * {@code Please set a name}.
+     * Key {@code HelloWorldBuilder.DescriptorImpl.DisplayName}: {@code
+     * Jenkins CFT Plugin}.
      * 
      * @return
-     *     {@code Please set a name}
+     *     {@code Jenkins CFT Plugin}
      */
-    public static String HelloWorldBuilder_DescriptorImpl_errors_missingName() {
-        return holder.format("HelloWorldBuilder.DescriptorImpl.errors.missingName");
+    public static String HelloWorldBuilder_DescriptorImpl_DisplayName() {
+        return holder.format("HelloWorldBuilder.DescriptorImpl.DisplayName");
     }
 
     /**
-     * Key {@code HelloWorldBuilder.DescriptorImpl.errors.missingName}:
-     * {@code Please set a name}.
+     * Key {@code HelloWorldBuilder.DescriptorImpl.DisplayName}: {@code
+     * Jenkins CFT Plugin}.
      * 
      * @return
-     *     {@code Please set a name}
+     *     {@code Jenkins CFT Plugin}
      */
-    public static Localizable _HelloWorldBuilder_DescriptorImpl_errors_missingName() {
-        return new Localizable(holder, "HelloWorldBuilder.DescriptorImpl.errors.missingName");
-    }
-
-    /**
-     * Key {@code HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench}:
-     * {@code Are you actually French?}.
-     * 
-     * @return
-     *     {@code Are you actually French?}
-     */
-    public static String HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench() {
-        return holder.format("HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench");
-    }
-
-    /**
-     * Key {@code HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench}:
-     * {@code Are you actually French?}.
-     * 
-     * @return
-     *     {@code Are you actually French?}
-     */
-    public static Localizable _HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench() {
-        return new Localizable(holder, "HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench");
+    public static Localizable _HelloWorldBuilder_DescriptorImpl_DisplayName() {
+        return new Localizable(holder, "HelloWorldBuilder.DescriptorImpl.DisplayName");
     }
 
     /**
@@ -92,25 +70,47 @@ public class Messages {
     }
 
     /**
-     * Key {@code HelloWorldBuilder.DescriptorImpl.DisplayName}: {@code Say
-     * hello world}.
+     * Key {@code HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench}:
+     * {@code Are you actually French?}.
      * 
      * @return
-     *     {@code Say hello world}
+     *     {@code Are you actually French?}
      */
-    public static String HelloWorldBuilder_DescriptorImpl_DisplayName() {
-        return holder.format("HelloWorldBuilder.DescriptorImpl.DisplayName");
+    public static String HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench() {
+        return holder.format("HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench");
     }
 
     /**
-     * Key {@code HelloWorldBuilder.DescriptorImpl.DisplayName}: {@code Say
-     * hello world}.
+     * Key {@code HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench}:
+     * {@code Are you actually French?}.
      * 
      * @return
-     *     {@code Say hello world}
+     *     {@code Are you actually French?}
      */
-    public static Localizable _HelloWorldBuilder_DescriptorImpl_DisplayName() {
-        return new Localizable(holder, "HelloWorldBuilder.DescriptorImpl.DisplayName");
+    public static Localizable _HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench() {
+        return new Localizable(holder, "HelloWorldBuilder.DescriptorImpl.warnings.reallyFrench");
+    }
+
+    /**
+     * Key {@code HelloWorldBuilder.DescriptorImpl.errors.missingName}:
+     * {@code Please set a name}.
+     * 
+     * @return
+     *     {@code Please set a name}
+     */
+    public static String HelloWorldBuilder_DescriptorImpl_errors_missingName() {
+        return holder.format("HelloWorldBuilder.DescriptorImpl.errors.missingName");
+    }
+
+    /**
+     * Key {@code HelloWorldBuilder.DescriptorImpl.errors.missingName}:
+     * {@code Please set a name}.
+     * 
+     * @return
+     *     {@code Please set a name}
+     */
+    public static Localizable _HelloWorldBuilder_DescriptorImpl_errors_missingName() {
+        return new Localizable(holder, "HelloWorldBuilder.DescriptorImpl.errors.missingName");
     }
 
 }
